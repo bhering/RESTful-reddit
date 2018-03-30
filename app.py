581e4356baaffe893ad153230dd14261064dc122
Gabriel Bhering <bhering@dcc.ufrj.br>
2018-03-30 02:22:45 -0300

    tiny cosmetic change for handling 404

--- app.py
+++ app.py
@@ -3,10 +3,19 @@ from flask import Flask, jsonify
 from info import info
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS']=False
 
 @app.route('/')
 def index():
     return jsonify(info)
+
+@app.errorhandler(404)
+def page_not_found(error):
+	return jsonify(
+	{
+		'error':'this end point is not yet implemented',
+		'code':404
+	})
 
 if __name__ == '__main__':
     app.run(debug=True)
