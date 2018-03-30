e87b522f02a3014d428df382d60ccaf13e0f70fe
Gabriel Bhering <bhering@dcc.ufrj.br>
2018-03-30 09:20:10 -0300

    progress on the datafetcher, it does fetch data and extract it correctly, next step is getting it to insert it into the db at specified times

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,16 +1,39 @@
 import http.client
+import json
+from pprint import pprint
 
-conn = http.client.HTTPSConnection("api.reddit.com")
+def do_fetch(rest_arg='/r/artificial/hot'):
+	conn = http.client.HTTPSConnection("api.reddit.com")
 
-headers = {
-    'Cache-Control': "no-cache",
-    'User-agent': "bhering bot 0.2"
-    }
+	headers = {
+	   		'Cache-Control': "no-cache",
+    		'User-agent': "bhering bot 0.2"
+	    }
 
-conn.request("GET", "/r/artificial/hot", headers=headers)
+	conn.request("GET", rest_arg, headers=headers)
 
-res = conn.getresponse()
-data = res.read()
+	res = conn.getresponse()
+	data = res.read()
 
-print(res.status, res.reason)
-print(data.decode("utf-8"))
+	return json.loads(data.decode("utf-8")), res.status, res.reason
+
+
+def extract_posts(data):
+	posts,_,_=data
+	posts=posts['data']['children']
+	for post in posts:
+		post_data=post['data']
+		yield 	{
+					'title':post_data['title'],
+					'author':post_data['author'],
+					'timestamp':post_data['created_utc'],
+					'ups':post_data['ups'],
+					'downs':post_data['downs'],
+					'num_comments':post_data['num_comments'],
+				}
+
+
+if __name__=="__main__":
+	raw_data=do_fetch()
+	posts=[post for post in extract_posts(raw_data)]
+	pprint(posts)
