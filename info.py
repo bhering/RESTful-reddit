f83066befd6d3ff25a2009d932a4ac6849a23211
Gabriel Bhering <bhering@dcc.ufrj.br>
2018-03-30 01:03:10 -0300

    more initial stuff, added a very basic server, some info, a quick run script

---
+++ info.py
@@ -0,0 +1,13 @@
+info = {
+  "title": "gbhering's reddit hot topics api",
+  "description": "what says on the title, done as a test",
+  "contact": {
+    "name": "Gabriel Bhering",
+    "url": "https://github.com/bhering/RESTful-reddit",
+  },
+  "license": {
+    "name": "MIT",
+    "url": "https://opensource.org/licenses/MIT"
+  },
+  "version": "0.0.1"
+}
