d560c6a89cb5d61865691fba33fe6e755f749540
Gabriel Bhering <bhering@dcc.ufrj.br>
2018-03-30 21:53:19 -0300

    refactoring and further developing database code

--- app.py
+++ app.py
@@ -1,27 +1,54 @@
 #!flask/bin/python
 
+# imports here
 import click
-from flask import Flask, jsonify
+from flask import Flask, g, jsonify
 from info import info
 import os
 import sqlite3
 
 
+### app instantiation ###
+
 app = Flask(__name__)
 app.config.update({
 	'JSON_SORT_KEYS':False,
-	'DATABASE':os.path.join(app.instance_path, 'poney.db'),
+	'DATABASE':os.path.join(app.root_path, 'posts.db'),
 })
 
+### cli commands ###
 
-@app.cli.command()
-def initdb():
-    """Initialize the database."""
-    click.echo('Init the db')
+@app.cli.command('initdb')
+def init_db():
+	db = get_db()
+	with app.open_resource('schema.sql', mode='r') as f:
+		db.cursor().executescript(f.read())
+	db.commit()
+	click.echo('db started')
+
+
+### database stuff ###
+
+def connect_db():
+	print(app.config)
+	r=sqlite3.connect(app.config['DATABASE'])
+	r.row_factory=sqlite3.Row
+	return r
+
+
+def get_db():
+	if not hasattr(g, 'sqlite_db'):
+		g.sqlite_db=connect_db()
+		return g.sqlite_db
+
+### routing ###
 
 @app.route('/')
 def index():
     return jsonify(info)
+
+
+### error handling ###
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -30,6 +57,17 @@ def page_not_found(error):
 		'error':'this end point is not yet implemented',
 		'code':404
 	})
+
+
+### teardown ###
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
+
+
+### just in case ###
 
 if __name__ == '__main__':
     app.run(debug=True)
